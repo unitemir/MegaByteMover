66ada1cde05dd0c1a52dda589f4959f1f1027210
unitemir <unitemir@gmail.com>
2023-10-13 10:26:13 +0600

    Refactor S3 multipart upload in FastAPI - Reorganized imports and logging. - Streamlined multipart upload logic.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI, UploadFile, HTTPException, Depends, File
-import boto3
+from fastapi import FastAPI, UploadFile, File
 from starlette.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from typing import List
 import logging
+import boto3
 
 logging.basicConfig(filename='app.log', level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
@@ -17,44 +19,64 @@ app.add_middleware(
 )
 
 BUCKET_NAME = 'fastapi-fileuploader-2023'
-CHUNK_SIZE = 1024 * 1024
 
 s3 = boto3.client('s3',
                   endpoint_url='http://localhost:9000',
-                  aws_access_key_id='minioadmin',
-                  aws_secret_access_key='minioadmin',
+                  aws_access_key_id='admin',
+                  aws_secret_access_key='password',
                   config=boto3.session.Config(signature_version='s3v4'))
 
+class Part(BaseModel):
+    PartNumber: int
+    ETag: str
 
-class FileWrapper:
-    def __init__(self, file, chunk_size=1024*1024):
-        self._file = file
-        self._chunk_size = chunk_size
-        self._buffer = b""
 
-    def read(self, size=-1):
-        while size > len(self._buffer):
-            chunk = self._file.read(self._chunk_size)
-            if not chunk:
-                break
-            self._buffer += chunk
-
-        part = self._buffer[:size]
-        self._buffer = self._buffer[size:]
-        return part
+class CompleteUploadRequest(BaseModel):
+    filename: str
+    upload_id: str
+    parts: List[Part]
 
 
 @app.post("/upload/")
-async def upload(file: UploadFile = File(...)):
-    logger.info(f"Starting upload for file: {file.filename}")
+async def upload_part(filename: str, part_number: int, upload_id: str, file: UploadFile = File(...)):
+    logger.info(f"Initiating upload for file: {filename}, part number: {part_number}, upload ID: {upload_id}")
+    part = file.file.read()
+
+    response = s3.upload_part(
+        Body=part,
+        Bucket=BUCKET_NAME,
+        Key=filename,
+        PartNumber=part_number,
+        UploadId=upload_id
+    )
+
+    logger.info(f"Part uploaded. ETag: {response['ETag']}")
+    return {"etag": response["ETag"]}
+
+
+@app.post("/upload/initiate/")
+def initiate_upload(filename: str):
+    logger.info(f"Initiating multipart upload for file: {filename}")
+    response = s3.create_multipart_upload(Bucket=BUCKET_NAME, Key=filename)
+    logger.info(f"Multipart upload initiated with ID: {response['UploadId']}")
+    return {"upload_id": response["UploadId"]}
+
+
+@app.post("/upload/complete/")
+def complete_upload(request: CompleteUploadRequest):
+    logger.info(
+        f"Received request for complete_upload with filename: {request.filename}, upload_id: {request.upload_id}, parts: {request.parts}")
+    parts_as_dicts = [{'PartNumber': part.PartNumber, 'ETag': part.ETag} for part in request.parts]
 
     try:
-        wrapped_file = FileWrapper(file.file)
-        s3.upload_fileobj(wrapped_file, BUCKET_NAME, file.filename)
-
-        logger.info(f"Successfully uploaded file: {file.filename}")
-        return {"filename": file.filename, "status": "uploaded"}
-
+        s3.complete_multipart_upload(
+            Bucket=BUCKET_NAME,
+            Key=request.filename,
+            UploadId=request.upload_id,
+            MultipartUpload={"Parts": parts_as_dicts}
+        )
+        logger.info("Multipart upload completed successfully.")
+        return {"status": "completed"}
     except Exception as e:
-        logger.error(f"Failed to upload file: {file.filename}. Error: {str(e)}")
-        raise HTTPException(status_code=500, detail=str(e))
+        logger.error(f"Error while completing multipart upload: {e}")
+        raise e
